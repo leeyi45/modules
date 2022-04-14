dd51a27b4f0fe587d7630d6893f4dd4e11540fb9
leeyi45 <leeyi45@gmail.com>
2022-04-14 16:38:54 +0800

    Update index.js
    On building documentation, convert the json files into HTML friendly format so that js-slang doesn't have to keep reparsing it on the fly

--- scripts/docs/index.js
+++ scripts/docs/index.js
@@ -1,7 +1,9 @@
+const chalk = require('chalk');
+const fs = require('fs');
 const TypeDoc = require('typedoc');
 const paths = require('./paths');
 const modules = require('../../modules.json');
-const fs = require('fs');
+const drawdown = require('./drawdown');
 
 const app = new TypeDoc.Application();
 app.options.addReader(new TypeDoc.TSConfigReader());
@@ -44,32 +46,119 @@ async function build_json(module) {
 
   const project = app.convert();
 
-  if (project) {
-    // Because typedoc clears the output directory every time it outputs,
-    // we make a new directory to temporarily store the output for each module
-    const outputDir = `build/documentation/${module}`;
-    fs.mkdir(outputDir, { recursive: true }, errHandler);
-    await app.generateJson(project, outputDir + `/documentation.json`);
-  }
+  if (!project) return undefined;
+
+  // Because typedoc clears the output directory every time it outputs,
+  // we make a new directory to temporarily store the output for each module
+  fs.mkdir(`build/documentation/${module}`, { recursive: true }, errHandler);
+  await app.generateJson(
+    project,
+    `build/documentation/${module}/documentation.json`
+  );
+
+  // Read the json output from typedoc and format it into html
+  fs.readFile(
+    `build/documentation/${module}/documentation.json`,
+    'utf-8',
+    (err, data) => {
+      if (err) console.error(err);
+      else {
+        const docs = JSON.parse(data).children;
+
+        let output;
+
+        if (docs) {
+          output = docs.reduce((result, element) => {
+            if (element.kindString === 'Variable') {
+              let desc = element.comment?.shortText;
+              if (!desc) {
+                desc = element.name;
+                console.warn(
+                  `${chalk.yellow(
+                    'Warning:'
+                  )} ${module}: No description found for ${element.name}`
+                );
+              } else {
+                desc = drawdown(desc);
+              }
+
+              const typeStr =
+                element.type?.name !== undefined ? `:${element.type.name}` : '';
+
+              result[
+                element.name
+              ] = `<div><h4>${element.name}${typeStr}</h4><div class="description">${desc}</div></div>`;
+            } else if (element.kindString === 'Function') {
+              // If the documentation is for a function
+              if (!element.signatures || element.signatures[0] === undefined)
+                throw new Error(
+                  `${chalk.red(
+                    'Error:'
+                  )} ${module}: Unable to find a signature for function ${
+                    element.name
+                  }!`
+                );
+
+              // In source all functions should only have one signature
+              const signature = element.signatures[0];
+
+              // Form the parameter string for the function
+              let paramStr;
+              if (!signature.parameters) paramStr = `()`;
+              else
+                paramStr = `(${signature.parameters
+                  .map((param) => param.name)
+                  .join(', ')})`;
+
+              // Form the result representation for the function
+              let resultStr;
+              if (!signature.type) resultStr = `void`;
+              else resultStr = signature.type.name;
+
+              let desc = signature.comment?.shortText;
+              if (!desc) {
+                desc = element.name;
+                console.warn(
+                  `${module}: No description found for signature of ${element.name}`
+                );
+              } else {
+                desc = drawdown(desc);
+              }
+
+              result[
+                element.name
+              ] = `<div><h4>${element.name}${paramStr} → {${resultStr}}</h4><div class="description">${desc}</div></div>`;
+            }
+            return result;
+          }, {});
+        } else {
+          // If there was no documentation for that module, just
+          // return nothing
+          output = {};
+        }
+
+        fs.writeFile(
+          `build/jsons/${module}.json`,
+          JSON.stringify(output, null, 2),
+          (err) => {
+            if (err) console.error(err);
+            fs.rm(
+              `build/documentation/${module}`,
+              { recursive: true, force: true },
+              errHandler
+            );
+          }
+        );
+      }
+    }
+  );
 }
 
 async function main() {
+  fs.rmSync(`${paths.root}/build/jsons`, { recursive: true, force: true });
   await build_all();
+  fs.mkdirSync(`${paths.root}/build/jsons`);
   await Promise.all(Object.keys(modules).map(build_json));
-
-  // Copy each module's json file to the jsons folder which is used
-  // by js-slang to import documentation
-  const dir = `${paths.root}/build/documentation`;
-  fs.mkdir(`${dir}/jsons`, errHandler);
-  for (const module of Object.keys(modules)) {
-    fs.rename(
-      `${dir}/${module}/documentation.json`,
-      `${dir}/jsons/${module}.json`,
-      errHandler
-    );
-
-    fs.rm(`${dir}/${module}`, { recursive: true, force: true }, errHandler);
-  }
 }
 
 main().catch(console.error);
